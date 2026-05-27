f43ed7054fb926b230126c0922e2743d21c140f6
Daniel Gasson <24572@burnside.school.nz>
2026-05-27 14:52:52 +1200

    fixed the problem with all the data bunching up by adding spacing between them

--- database.py
+++ database.py
@@ -13,7 +13,7 @@ def print_all_aircraft():
     cursor.execute(sql)
     results = cursor.fetchall()
     for airnzaircraft in results:
-        print(airnzaircraft)
+        print(f"{airnzaircraft[1]:<20}"f"{airnzaircraft[2]:<8}"f"{airnzaircraft[3]:<8}"f"{airnzaircraft[4]:<8}"f"{airnzaircraft[5]:<8}"f"{airnzaircraft[6]:<5}"f"{airnzaircraft[7]:<5}"f"{airnzaircraft[8]:<5}")
     db.close()
 
 
