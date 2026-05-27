c5f969624bcabb5c3d84245b09cf2a102ab8c28b
Daniel Gasson <24572@burnside.school.nz>
2026-05-27 15:07:54 +1200

    added function to print all aircraft by most in the fleet

--- database.py
+++ database.py
@@ -29,15 +29,29 @@ def print_all_aircraft_by_speed():
         print(f"{airnzaircraft[1]:<20}"f"{airnzaircraft[2]:<15}"f"{airnzaircraft[3]:<15}"f"{airnzaircraft[4]:<12}"f"{airnzaircraft[5]:<12}"f"{airnzaircraft[6]:<15}"f"{airnzaircraft[7]:<12}"f"{airnzaircraft[8]:<12}")
     db.close()
 
+def print_all_aircraft_by_number():
+    #prints all aircraft data sorted by speed
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "select * from airnzaircraft order by no_of_aircraft desc;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    print(f"{'Name':<20}"f"{'passengers':<15}"f"{'Range':<15}"f"{'Speed':<12}"f"{'Max weight':<12}"f"{'takeoff speed':<15}"f"{'land speed':<12}"f"{'no of planes':<12}")
+    for airnzaircraft in results:
+        print(f"{airnzaircraft[1]:<20}"f"{airnzaircraft[2]:<15}"f"{airnzaircraft[3]:<15}"f"{airnzaircraft[4]:<12}"f"{airnzaircraft[5]:<12}"f"{airnzaircraft[6]:<15}"f"{airnzaircraft[7]:<12}"f"{airnzaircraft[8]:<12}")
+    db.close()
 
 
-data = input("What would you like to learn about Air NZ planes?\n1. print all aircraft\n2. print aircraft by speed\n3. exit\n")
+
+data = input("What would you like to learn about Air NZ planes?\n1. print all aircraft\n2. print aircraft by speed\n3. print all aircraft by the highest amount in the fleet\n4. leave\n")
 if data == "1":
     print_all_aircraft()
 elif data == "2":
     print_all_aircraft_by_speed()
 elif data == "3":
-    print("Goodbye!")
+    print_all_aircraft_by_number()
+elif data == "4":
+    print("goodbye")
 
 
 
