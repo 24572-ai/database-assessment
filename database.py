1c3fc9348c51742f86d1a8564aaace82d7e40b37
Daniel Gasson <24572@burnside.school.nz>
2026-05-27 14:23:49 +1200

    added docstring and database variable

--- database.py
+++ database.py
@@ -1,9 +1,21 @@
-import sqlite3
+#docsting - this is code to extract data from a database of airnzaircraft made by daniel gasson
 
-db = sqlite3.connect('airnzairplanes.db')
+
+
+import sqlite3
+DATABASE = 'airnzairplanes.db'
+
+
+
+
+
+db = sqlite3.connect(DATABASE)
 cursor = db.cursor()
-sql = "SELECT * FROM airnzaircraft"
+sql = "select * from airnzaircraft;"
 cursor.execute(sql)
 results = cursor.fetchall()
 print(results)
+
+
+
 db.close()
