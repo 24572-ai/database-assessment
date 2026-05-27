8998680d1c93de880345eaa22dc1ef45d600d11c
Daniel Gasson <24572@burnside.school.nz>
2026-05-27 14:43:51 +1200

    modified the code to print more organised with each aircraft in individual lines

--- database.py
+++ database.py
@@ -5,17 +5,21 @@
 import sqlite3
 DATABASE = 'airnzairplanes.db'
 
+def print_all_aircraft():
+    #prints all aircraft data in a readable way
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "select * from airnzaircraft;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    for airnzaircraft in results:
+        print(airnzaircraft)
+    db.close()
+
+
+
+print_all_aircraft()
 
 
 
 
-db = sqlite3.connect(DATABASE)
-cursor = db.cursor()
-sql = "select * from airnzaircraft;"
-cursor.execute(sql)
-results = cursor.fetchall()
-print(results)
-
-
-
-db.close()
